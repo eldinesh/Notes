66cbe0230aa05d00e24a436c8b0f71f1cfff6c7d
Dinesh Kumar <eldineshk@gmail.com>
2022-11-08 23:26:16 +0530

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Dinesh Kumar on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
